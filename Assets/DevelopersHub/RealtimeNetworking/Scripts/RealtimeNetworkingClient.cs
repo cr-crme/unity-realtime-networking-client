4fa6f9d8c4ce41f798ffbdf2512310eaf4a09508
Pariterre <pariterre@hotmail.com>
2024-05-02 16:51:19 -0400

    Added panel to connect to server in client

--- Assets/DevelopersHub/RealtimeNetworking/Scripts/RealtimeNetworkingClient.cs
+++ Assets/DevelopersHub/RealtimeNetworking/Scripts/RealtimeNetworkingClient.cs
@@ -3,25 +3,32 @@ namespace DevelopersHub.RealtimeNetworking.Client
 {
     using System.Collections;
     using System.Collections.Generic;
+    using System.Text.RegularExpressions;
     using DevelopersHub.RealtimeNetworking.Common;
     using UnityEngine;
+    using UnityEngine.UI;
 
     public class RealtimeNetworkingClient : MonoBehaviour
     {        
         [SerializeField] private List<Transform> _objectsToMove;
-        [SerializeField] private string _serverIp = "127.0.0.1";
-        [SerializeField] private int _serverPort = 5555;
 
+        [SerializeField] private Canvas _connexionPanel;
+        [SerializeField] private InputField _serverIpAddressInput;
+        [SerializeField] private int _serverPort = 5555;
+        [SerializeField] private Button _connectButton;
+        [SerializeField] private Button _cancelConnectButton;
+
+        private bool _isConnecting = false;
         private bool _isConnected = false;
         private float _timeStamp = 0.0f; 
 
         // Start is called before the first frame update
         void Start()
         {
-            RealtimeNetworking.OnDisconnectedFromServer += TryConnecting;
+            RealtimeNetworking.OnDisconnectedFromServer += OnConnexionLost;
             RealtimeNetworking.OnPacketReceived += OnPacketReceived;
 
-            TryConnecting();
+            ValidateIpAddress();
         }
 
         // Update is called once per frame
@@ -51,30 +58,69 @@ namespace DevelopersHub.RealtimeNetworking.Client
             } catch (System.Exception e)
             {
                 Debug.Log("Connection lost");
-                TryConnecting();
+                OnConnexionLost();
             }
 
             _timeStamp += Time.fixedDeltaTime;
         }
 
+        public void ValidateIpAddress()
+        {
+            if (_serverIpAddressInput == null)
+            {
+                _connectButton.interactable = false;
+                return;
+            }
+
+            _serverIpAddressInput.text = _serverIpAddressInput.text.Trim();
+            if (string.IsNullOrEmpty(_serverIpAddressInput.text))
+            {
+                _connectButton.interactable = false;
+                return;
+            }
+
+            string pattern = @"^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+            Regex regex = new Regex(pattern);
+            if (!regex.IsMatch(_serverIpAddressInput.text))
+            {
+                _connectButton.interactable = false;
+                return;
+            }
+
+            _connectButton.interactable = true;
+        }
+
         void OnConnectionResult(bool success)
         {
             _isConnected = success;
-
-            if (_isConnected)
-            {
-                Debug.Log("Connected to server");
-                RealtimeNetworking.OnConnectingToServerResult -= OnConnectionResult;
-            }
         }
 
-        void TryConnecting()
+        void OnConnexionLost()
         {
+            _isConnecting = false;
             _isConnected = false;
+
+            _connexionPanel.gameObject.SetActive(true);
+        }
+
+        public void TryConnecting()
+        {
+            _serverIpAddressInput.interactable = false;
+            _connectButton.gameObject.SetActive(false);
+            _cancelConnectButton.gameObject.SetActive(true);
+
+            _isConnecting = true;
+            _isConnected = false;
+
             _timeStamp = 0.0f;
             RealtimeNetworking.OnConnectingToServerResult += OnConnectionResult;
 
             StartCoroutine(TryConnectingCoroutine());
+        }
+
+        public void CancelTryConnecting()
+        {
+            _isConnecting = false;
         }
 
         System.Collections.IEnumerator TryConnectingCoroutine()
@@ -82,14 +128,27 @@ namespace DevelopersHub.RealtimeNetworking.Client
             // Just make sure it is disconnected
             RealtimeNetworking.Disconnect();
 
-            while (!_isConnected)
+            while (!_isConnected && _isConnecting)
             {
                 Debug.Log("Trying to Connect...");
-                RealtimeNetworking.Connect(_serverIp, _serverPort);
+                RealtimeNetworking.Connect(_serverIpAddressInput.text, _serverPort);
                 // Pause for a second before trying to reconnect
                 yield return new WaitForSeconds(1);
             }
 
+            _isConnecting = false;
+
+            RealtimeNetworking.OnConnectingToServerResult -= OnConnectionResult;
+
+            _serverIpAddressInput.interactable = true;
+            _connectButton.gameObject.SetActive(true);
+            _cancelConnectButton.gameObject.SetActive(false);
+            
+            if (_isConnected)
+            {
+                Debug.Log("Connected to server");
+                _connexionPanel.gameObject.SetActive(false);
+            }
         }
 
         void OnPacketReceived(Packet packet)
